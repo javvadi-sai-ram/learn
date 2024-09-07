26581115fd5409d73c50d9c7be160444d5b33b02
sairamjavvadi <sairamjavvadi@gmail.com>
2024-09-07 10:58:02 -0500

    comment add to commit

--- src/App/Store/Store.js
+++ src/App/Store/Store.js
@@ -1,6 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import resumeReducer from '../createSlice/createSlice';
 
+//sajdhskj
+
 const store = configureStore({
   reducer: {
     saiResume: resumeReducer,
